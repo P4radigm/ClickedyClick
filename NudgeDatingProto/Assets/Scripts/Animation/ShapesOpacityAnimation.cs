b223c4e58e9f8df3a131333d2c36faab4f5e547f
Leon van Oldenborgh <leonvano@xs4all.nl>
2023-11-08 11:02:16 +0100

    added windows + xcode project builds

--- NudgeDatingProto/Assets/Scripts/Animation/ShapesOpacityAnimation.cs
+++ NudgeDatingProto/Assets/Scripts/Animation/ShapesOpacityAnimation.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Shapes;
-using static UnityEditor.Timeline.TimelinePlaybackControls;
 
 [RequireComponent(typeof(ShapeRenderer))]
 public class ShapesOpacityAnimation : MonoBehaviour
